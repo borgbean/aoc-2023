a89f4cdd27bf78a9002d2b9f832989c0464cd70d
Michael Martin <borgbean@sammich.info>
2023-12-06 00:46:40 -0500

    day 6

---
+++ 12.js
@@ -0,0 +1,78 @@
+let input = ``;
+// input = ``;
+
+
+
+function doIt(input) {
+    input = input.split('\n');
+    
+    let times = [Number(input[0].split(':')[1].trim().replaceAll(/\s/g, ''))];
+    let distances = [Number(input[1].split(':')[1].trim().replaceAll(/\s/g, ''))+1];
+    
+    let product = 1;
+
+    for(let race = 0; race < times.length; ++race) {
+        let t = times[race];
+        let d = distances[race];
+
+        let minHold = 1;
+        let maxHold = t-1;
+        
+
+        let cmp = (holdT, reverse) => {
+            let guessDist = getTravelled(t, holdT);
+            if(guessDist < d) {
+                let guessDist2 = getTravelled(t, holdT + 1);
+                if(guessDist2 > guessDist) {
+                    return -1;
+                }
+                return 1;
+            }
+            
+            let direction = reverse ? -1 : 1;
+            
+            let guessDist2 = getTravelled(t, holdT - direction);
+            if(guessDist2 < d) {
+                return 0;
+            }
+            return direction;
+        }
+        let holdL = binarySearch(minHold, maxHold, cmp);
+        let holdR = binarySearch(minHold, maxHold, holdT => cmp(holdT, true));
+
+        product *= (1+holdR-holdL);
+    }
+
+    return product;
+}
+
+
+console.log(doIt(input));
+
+
+function getTravelled(t, holdT) {
+    let v = holdT;
+    let tLeft = t - holdT;
+    let travelled = v*tLeft;
+
+    return travelled;
+}
+
+function binarySearch(l, r, cmp) {
+    while(l < r) {
+        let mid = Math.floor((l+r)/2);
+
+        let c = cmp(mid);
+        if(c < 0) {
+            l = mid + 1;
+        } else if(c > 0) {
+            r = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+
+    return l;
+}
+
+console.log(doIt(input));
